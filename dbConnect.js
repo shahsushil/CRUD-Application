8b0179a2dc5c8fb5ff0157aa96c582063a38e7b9
shahsushil <gsusheel15@gmail.com>
2025-11-25 11:26:23 +0530

    Adding .env file

--- dbConnect.js
+++ dbConnect.js
@@ -3,7 +3,7 @@ const mongoose=require("mongoose")
 
 //  by promise  / schema method
 
-mongoose.connect("mongodb://127.0.0.1:27017/wd2pmJuneCrud")
+mongoose.connect(process.env.MONGO_URI)
 .then(()=>{
     console.log("Database is Successfully connected");
 })
--- index.js
+++ index.js
@@ -109,6 +109,11 @@ app.get("/search",async(req,res)=>{
     }
 })
 
-app.listen(8000,()=>{
-    console.log("server is running at http://localhost:8000");
-})
+// app.listen(8000,()=>{
+//     console.log("server is running at http://localhost:8000");
+// })
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
